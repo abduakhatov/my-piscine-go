fc12d07166f8d1566be98cd6a5f93b1654fa2459
abduakhatov <student@ALEM-3170e2.alem.school>
2020-02-18 13:02:51 +0600

    8_cl_camp4

--- myfamily.sh
+++ myfamily.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl -s https://01.alem.school/assets/superhero/all.json | jq -c '.[] | select(.id == $id)' --argjson id $HERO_ID | jq -c '.connections.relatives' | tr -d '"/"'
+curl -s https://01.alem.school/assets/superhero/all.json | jq -c '.[] | select(.id == $id)' --argjson id $HERO_ID | jq -c '.connections.relatives' | tr -d '"/"' | tr -d '\"' | tr -d "\'"
