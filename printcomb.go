e8b023764aaccae1a2ecf179fad3740ce907de47
abduakhatov <student@ALEM-316ec9.alem.school>
2020-02-19 17:20:15 +0600

    t5

--- printcomb.go
+++ printcomb.go
@@ -3,13 +3,18 @@ package piscine
 import "github.com/01-edu/z01"
 
 func PrintComb() {
-	for i := '0'; i <= '9'; i++ {
-		for j := '0'; j <= '9'; j++ {
-			for k := '0'; k <= '9'; k++ {
+	min := '0'
+	max := '9'
+	for i := min; i <= max; i++ {
+		for j := min; j <= max; j++ {
+			for k := min; k <= max; k++ {
 				if i < j && j < k {
 					z01.PrintRune(i)
 					z01.PrintRune(j)
 					z01.PrintRune(k)
+					if k == max && j == k - 1 && i == j - 1 {
+						continue
+					} 
 					z01.PrintRune(',')
 					z01.PrintRune(' ')
 				}
