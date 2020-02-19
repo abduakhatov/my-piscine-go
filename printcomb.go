04cb5749f22c833e85f308e6e27a36cd76cac978
abduakhatov <student@ALEM-316ec9.alem.school>
2020-02-19 17:00:26 +0600

    t5

---
+++ printcomb.go
@@ -0,0 +1,20 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+func PrintComb() {
+	for i := '0'; i <= '9'; i++ {
+		for j := '0'; j <= '9'; j++ {
+			for k := '0'; k <= '9'; k++ {
+				if i < j && j < k {
+					z01.PrintRune(i)
+					z01.PrintRune(j)
+					z01.PrintRune(k)
+					z01.PrintRune(',')
+					z01.PrintRune(' ')
+				}
+			}
+		}
+	}
+	z01.PrintRune('\n')
+}
--- test/main.go
+++ test/main.go
@@ -3,7 +3,11 @@ package main
 import piscine ".."
 
 func main() {
-	piscine.IsNegative(1)
-	piscine.IsNegative(0)
-	piscine.IsNegative(-1)
+	// Task 4
+	// piscine.IsNegative(1)
+	// piscine.IsNegative(0)
+	// piscine.IsNegative(-1)
+
+	// Task 5
+	piscine.PrintComb()
 }
