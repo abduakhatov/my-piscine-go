c93b244fe936ddca9e0f9a804c4584842cf343f2
abduakhatov <student@ALEM-316ec9.alem.school>
2020-02-19 20:46:48 +0600

    asddsf

--- printcomb.go
+++ printcomb.go
@@ -5,18 +5,19 @@ import "github.com/01-edu/z01"
 func PrintComb() {
 	_min := '0'
 	_max := '9'
+	var st int = 0
 	for i := _min; i <= _max; i++ {
 		for j := _min; j <= _max; j++ {
 			for k := _min; k <= _max; k++ {
 				if i < j && j < k {
+					if st > 0 {
+						z01.PrintRune(',')
+						z01.PrintRune(' ')
+					}
 					z01.PrintRune(i)
 					z01.PrintRune(j)
 					z01.PrintRune(k)
-					if k == _max && j == k-1 && i == j-1 {
-						continue
-					}
-					z01.PrintRune(',')
-					z01.PrintRune(' ')
+					st = 1
 				}
 			}
 		}
