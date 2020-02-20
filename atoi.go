b50cbbf28d5dcdd9dbe2676c37482b45a863010a
abduakhatov <abduakhatov>
2020-02-21 00:01:48 +0600

    sdaf

--- atoi.go
+++ atoi.go
@@ -1,6 +1,8 @@
-package main
+package piscine
 
-func BasicAtoi2(s string) int {
+import "fmt"
+
+func Atoi(s string) int {
 	_s := []byte(s)
 	count := 0
 	for range _s {
@@ -9,13 +11,43 @@ func BasicAtoi2(s string) int {
 	result := 0
 	pow := 1
 	tmp := 0
+
+	
+
 	for i := count - 1; i >= 0; i-- {
 		tmp = (int(_s[i]) - 48)
-		if tmp < 0 || tmp > 9 {
-			return 0
-		}
+		
 		result += (tmp * pow)
 		pow *= 10
 	}
 	return result
+}
+
+func main() {
+	s := "12345"
+	s2 := "0000000012345"
+	s3 := "012 345"
+	s4 := "Hello World!"
+	s5 := "+1234"
+	s6 := "-1234"
+	s7 := "++1234"
+	s8 := "--1234"
+
+	n := Atoi(s)
+	n2 := Atoi(s2)
+	n3 := Atoi(s3)
+	n4 := Atoi(s4)
+	n5 := Atoi(s5)
+	n6 := Atoi(s6)
+	n7 := Atoi(s7)
+	n8 := Atoi(s8)
+
+	fmt.Println(n)
+	fmt.Println(n2)
+	fmt.Println(n3)
+	fmt.Println(n4)
+	fmt.Println(n5)
+	fmt.Println(n6)
+	fmt.Println(n7)
+	fmt.Println(n8)
 }
--- test/main.go
+++ test/main.go
@@ -6,7 +6,19 @@ import (
 )
 
 func main() {
-	s := "Hello World!"
-	s = piscine.StrRev(s)
-	fmt.Println(s)
+	s := "12345"
+	s2 := "0000000012345"
+	s3 := "012 345"
+	s4 := "Hello World!"
+
+	n := piscine.BasicAtoi2(s)
+	n2 := piscine.BasicAtoi2(s2)
+	n3 := piscine.BasicAtoi2(s3)
+	n4 := piscine.BasicAtoi2(s4)
+
+	fmt.Println(n)
+	fmt.Println(n2)
+	fmt.Println(n3)
+	fmt.Println(n4)
+
 }
