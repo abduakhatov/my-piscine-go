ec1ac6b4e9c66e10105df3c68024e4ecd1e26ebd
abduakhatov <student@ALEM-a1d7c6.alem.school>
2020-02-20 18:01:04 +0600

    task1

--- test/main.go
+++ test/main.go
@@ -6,7 +6,11 @@ import (
 )
 
 func main() {
-    n := 0
-    piscine.PointOne(&n)
-    fmt.Println(n)
+	a := 13
+	b := 2
+	var div int
+	var mod int
+	piscine.DivMod(a, b, &div, &mod)
+	fmt.Println(div)
+	fmt.Println(mod)
 }
