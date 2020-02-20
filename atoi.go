05ec4b7d87ec0da7e9a620550b3ff3f703f9a631
abduakhatov <abduakhatov>
2020-02-21 00:22:16 +0600

    atoi

--- atoi.go
+++ atoi.go
@@ -1,6 +1,4 @@
-package main
-
-import "fmt"
+package package
 
 func Atoi(s string) int {
 	_s := []byte(s)
@@ -28,33 +26,4 @@ func Atoi(s string) int {
 		}
 	}
 	return result * sign
-}
-
-func main() {
-	s := "12345"
-	s2 := "0000000012345"
-	s3 := "012 345"
-	s4 := "Hello World!"
-	s5 := "+1234"
-	s6 := "-1234"
-	s7 := "++1234"
-	s8 := "--1234"
-
-	n := Atoi(s)
-	n2 := Atoi(s2)
-	n3 := Atoi(s3)
-	n4 := Atoi(s4)
-	n5 := Atoi(s5)
-	n6 := Atoi(s6)
-	n7 := Atoi(s7)
-	n8 := Atoi(s8)
-
-	fmt.Println(n)
-	fmt.Println(n2)
-	fmt.Println(n3)
-	fmt.Println(n4)
-	fmt.Println(n5)
-	fmt.Println(n6)
-	fmt.Println(n7)
-	fmt.Println(n8)
 }
