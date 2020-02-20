3731ce72aff243190f53bd8654451895f19fe417
abduakhatov <student@ALEM-a1d7c6.alem.school>
2020-02-20 13:15:29 +0600

    PRINTNBR

---
+++ printnbr.go
@@ -0,0 +1,33 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+func PrintNbr(n int) {
+	dot := 1
+	if n == 0 {
+		z01.PrintRune('0')
+		z01.PrintRune('\n')
+		return
+	}
+	if n < 0 {
+		dot *= -1
+		z01.PrintRune('-')
+	}
+	_n := n
+	length := 0
+	for _n*dot > 0 {
+		_n /= 10
+		length += 1
+	}
+	n_arr := make([]int, length)
+	digit := 0
+	for i := length - 1; n*dot >= 1; i-- {
+		digit = int((n % 10) * dot)
+		n /= 10
+		n_arr[i] = digit
+	}
+	for i := 0; i < length; i++ {
+		z01.PrintRune(48 + rune(n_arr[i]))
+	}
+	z01.PrintRune('\n')
+}
