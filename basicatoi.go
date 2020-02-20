c7d84f24736751daafd1ca3dbe8e21ff65492355
abduakhatov <student@ALEM-a1d7c6.alem.school>
2020-02-20 21:47:44 +0600

    basic atoi

---
+++ basicatoi.go
@@ -0,0 +1,16 @@
+package piscine
+
+func BasicAtoi(s string) int {
+	_s := []byte(s)
+	count := 0
+	for range _s {
+		count += 1
+	}
+	result := 0
+	pow := 1
+	for i := count-1; i >= 0; i-- {
+		result += ((int(_s[i]) - 48) * pow)
+		pow *= 10
+	}
+	return result
+}
