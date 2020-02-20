160c1ba77956a3049db4061b16ae8a75b8ddbc15
abduakhatov <abduakhatov>
2020-02-21 01:26:36 +0600

    last

---
+++ sortintegertable.go
@@ -0,0 +1,16 @@
+package piscine
+
+import "fmt"
+
+func SortIntegerTable(table []int) {
+	for i := 0; i < len(table)-1; i++ {
+		for j := i+1; j < len(table); j++ {
+			if table[i] > table[j]{
+				tmp := table[i]
+				table[i] = table[j]
+				table[j] = tmp
+			}
+		}
+	}
+	fmt.Println(table)
+}
--- test/main.go
+++ test/main.go
@@ -7,33 +7,7 @@ import (
 
 
 func main() {
-	s := "12345"
-	s2 := "0000000012345"
-	s3 := "012 345"
-	s4 := "Hello World!"
-	s5 := "+1234"
-	s6 := "-1234"
-	s7 := "++1234"
-	s8 := "--1234"
-	s9 := "123-"
-
-	n := piscine.Atoi(s)
-	n2 := piscine.Atoi(s2)
-	n3 := piscine.Atoi(s3)
-	n4 := piscine.Atoi(s4)
-	n5 := piscine.Atoi(s5)
-	n6 := piscine.Atoi(s6)
-	n7 := piscine.Atoi(s7)
-	n8 := piscine.Atoi(s8)
-	n9 := piscine.Atoi(s9)
-
-	fmt.Println(n)
-	fmt.Println(n2)
-	fmt.Println(n3)
-	fmt.Println(n4)
-	fmt.Println(n5)
-	fmt.Println(n6)
-	fmt.Println(n7)
-	fmt.Println(n8)
-	fmt.Println(n9)
+	s := []int{5,4,3,2,1,0}
+	piscine.SortIntegerTable(s)
+	// fmt.Println(s)
 }
