690b96612e62c598665a5d5df8c29d4631ea083a
abduakhatov <abduakhatov>
2020-02-22 16:28:43 +0600

    raid Shohruh all

--- printcomb2.go
+++ printcomb2.go
@@ -6,13 +6,15 @@ func PrintComb2() {
 	_min := '0'
 	_max := '9'
 	var st int = 0
+	l := '0'
 	for i := _min; i <= _max; i++ {
 		for j := _min; j <= _max; j++ {
 			for k := i; k <= _max; k++ {
-				for l := _min; l <= _max; l++ {
-					if i == k && (j > l || j == l) {
-						continue
-					}
+				l = j + 1
+				for ; l <= _max; l++ {
+					// if i == k && (j > l || j == l) {
+					// 	continue
+					// }
 					if st > 0 {
 						z01.PrintRune(',')
 						z01.PrintRune(' ')
