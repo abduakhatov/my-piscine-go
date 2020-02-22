db191e385ae094962f308457ef8fef8c6b7ebe8c
abduakhatov <abduakhatov>
2020-02-22 16:37:48 +0600

    raid Shohruh all

--- printcomb2.go
+++ printcomb2.go
@@ -1,4 +1,4 @@
-package package
+package piscine
 
 import "github.com/01-edu/z01"
 
@@ -6,15 +6,13 @@ func PrintComb2() {
 	_min := '0'
 	_max := '9'
 	var st int = 0
-	l := '0'
 	for i := _min; i <= _max; i++ {
 		for j := _min; j <= _max; j++ {
 			for k := i; k <= _max; k++ {
-				l = j
-				for ; l <= _max; l++ {
-					// if i == k && (j > l || j == l) {
-					// 	continue
-					// }
+				for l := _min; l <= _max; l++ {
+					if i == k && (j > l || j == l) {
+						continue
+					}
 					if st > 0 {
 						z01.PrintRune(',')
 						z01.PrintRune(' ')
