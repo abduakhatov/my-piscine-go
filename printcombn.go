bdca33a513bef1d2178cc395082d3be2512c3600
abduakhatov <abduakhatov>
2020-02-22 22:15:44 +0600

    refactor

--- printcombn.go
+++ printcombn.go
@@ -4,7 +4,7 @@ import "github.com/01-edu/z01"
 
 func ValidComb(comb int) bool {
 	for comb != 0 {
-		if comb >= 10 && comb % 10 <= (comb / 10) % 10 {
+		if comb >= 10 && comb%10 <= (comb/10)%10 {
 			return false
 		}
 		comb /= 10
@@ -20,7 +20,7 @@ func PrintNext(numb int) {
 	if numb >= 10 {
 		PrintNext(numb / 10)
 	}
-	z01.PrintRune(48 + rune(numb % 10))
+	z01.PrintRune(48 + rune(numb%10))
 }
 
 func PrintCombN(n int) {
@@ -29,7 +29,7 @@ func PrintCombN(n int) {
 		postionLength *= 10
 	}
 	st := false
-	for i := postionLength / 10; i <= postionLength * 9; i++ {
+	for i := postionLength / 10; i <= postionLength*9; i++ {
 		if !ValidComb(i) {
 			continue
 		}
