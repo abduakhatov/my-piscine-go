ee0d00f0ff000432aaa54a6c71f8368cf433be95
abduakhatov <abduakhatov>
2020-02-22 12:43:32 +0600

    empty files-created

--- sortintegertable.go
+++ sortintegertable.go
@@ -2,8 +2,8 @@ package piscine
 
 func SortIntegerTable(table []int) {
 	for i := 0; i < len(table)-1; i++ {
-		for j := i+1; j < len(table); j++ {
-			if table[i] > table[j]{
+		for j := i + 1; j < len(table); j++ {
+			if table[i] > table[j] {
 				tmp := table[i]
 				table[i] = table[j]
 				table[j] = tmp
