8ad8704c34a09ae0332ef1bc35a2139ca0df8af8
abduakhatov <abduakhatov>
2020-02-24 13:53:59 +0600

    recursion4

---
+++ fibonacci.go
@@ -0,0 +1,12 @@
+package piscine
+
+func Fibonacci(index int) int {
+	return SubFibonacci(index, 0, 1)
+}
+
+func SubFibonacci(index,st,nd int) int {
+	if index == 1 {
+		return nd
+	}
+	return SubFibonacci(index-1, nd, st+nd)
+}
--- test/main.go
+++ test/main.go
@@ -1,14 +1,11 @@
 package main
 
-// import (
-// 	piscine ".."
-// )
-import "fmt"
+import (
+	"fmt"
+	piscine ".."
+)
 
 func main() {
-	i := 1650+2100+32200+27600+23000+18400+13800+9200+1533+1563+1250+1250+1250+1250+1100+1100+1100+1100+713+713+1375+950+950+713+713+600+600+1188+475+475+400+400+400+400+400+400+325+325+650+500+175
-	fmt.Println(i)
-	// piscine.PrintCombN(1)
-	// piscine.PrintCombN(3)
-	// piscine.PrintCombN(9)
+	arg1 := 4
+	fmt.Println(piscine.Fibonacci(arg1))
 }
