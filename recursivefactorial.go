aa5fa83873336fc0347d29bc2170a98dbadd3efc
abduakhatov <abduakhatov>
2020-02-24 16:19:06 +0600

    q4_t2

--- recursivefactorial.go
+++ recursivefactorial.go
@@ -1,9 +1,9 @@
 package piscine
 
 func RecursiveFactorial(nb int) int {
-	if nb < 0 || nb > 21{
+	if nb < 0 || nb > 21 {
 		return 0
-	} else if (nb <= 1) { 
+	} else if nb <= 1 { 
 		return 1
 	}
 	return RecursiveFactorial(nb-1) * nb
