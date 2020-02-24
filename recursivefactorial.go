ec140cba7540316747e9e8d5b339bc5f6344ead8
abduakhatov <abduakhatov>
2020-02-24 16:04:15 +0600

    q4_t2

--- recursivefactorial.go
+++ recursivefactorial.go
@@ -1,8 +1,17 @@
 package piscine
 
-func RecursiveFactorial(nb int) int {
-	if nb == 1 {
+func SubRecursiveFactorial(nb, index int) int {
+	if index > 21 {
+		return 0
+	} else if nb == 1 {
 		return 1
 	}
-	return RecursiveFactorial(nb-1) * nb
+	return SubRecursiveFactorial(nb-1, index+1) * nb
+}
+
+func RecursiveFactorial(nb int) int {
+	if nb < 0 {
+		return 0
+	}
+	return SubRecursiveFactorial(nb, 1)
 }
--- test/main.go
+++ test/main.go
@@ -7,14 +7,14 @@ import (
 
 func main() {
 	arg := 0
-	fmt.Println(piscine.IterativeFactorial(arg))
+	fmt.Println(piscine.RecursiveFactorial(arg))
 	arg = 2
-	fmt.Println(piscine.IterativeFactorial(arg))
+	fmt.Println(piscine.RecursiveFactorial(arg))
 	arg = 3
-	fmt.Println(piscine.IterativeFactorial(arg))
+	fmt.Println(piscine.RecursiveFactorial(arg))
 	arg = 4
-	fmt.Println(piscine.IterativeFactorial(arg))
+	fmt.Println(piscine.RecursiveFactorial(arg))
 	arg = 5
-	fmt.Println(piscine.IterativeFactorial(-8778634010981231238))
+	fmt.Println(piscine.RecursiveFactorial(-8778634010981231238))
 
 }
