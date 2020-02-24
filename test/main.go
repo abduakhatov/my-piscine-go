ee8d9f5baba9bdf2365889a5402f0c68803cc0b6
abduakhatov <abduakhatov>
2020-02-24 20:52:44 +0600

    afasdfhasgf

--- test/main.go
+++ test/main.go
@@ -11,9 +11,11 @@ func main() {
 	arg3 := -1
 	arg4 := 0
 	arg5 := 2147483647
+	arg6 := 1
 	fmt.Println(arg1, piscine.IsPrime(arg1))
 	fmt.Println(arg2, piscine.IsPrime(arg2))
 	fmt.Println(arg3, piscine.IsPrime(arg3))
 	fmt.Println(arg4, piscine.IsPrime(arg4))
 	fmt.Println(arg5, piscine.IsPrime(arg5))
+	fmt.Println(arg6, piscine.IsPrime(arg6))
 }
