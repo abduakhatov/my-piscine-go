8a9ed3830901ebf5f168387a160d9ad9449c1547
abduakhatov <abduakhatov>
2020-02-25 13:22:38 +0600

    Q5_t3

---
+++ nrune.go
@@ -0,0 +1,10 @@
+package piscine
+
+func NRune(s string, n int) rune {
+	for ind, val := range s {
+		if ind == n - 1 {
+			return rune(val)
+		}
+	}
+	return 0
+}
--- test/main.go
+++ test/main.go
@@ -6,8 +6,11 @@ import (
 )
 
 func main() {
-	z01.PrintRune(piscine.LastRune("Hello!"))
-	z01.PrintRune(piscine.LastRune("Salut!"))
-	z01.PrintRune(piscine.LastRune("Ola!"))
+	z01.PrintRune(piscine.NRune("Hello!", 3))
+	z01.PrintRune(piscine.NRune("Salut!", 2))
+	z01.PrintRune(piscine.NRune("Bye!", -1))
+	z01.PrintRune(piscine.NRune("Bye!", 5))
+	z01.PrintRune(piscine.NRune("Ola!", 4))
 	z01.PrintRune('\n')
 }
+
