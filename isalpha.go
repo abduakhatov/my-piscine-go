84a081001e129a5e1013f40735629395ddc6a82d
abduakhatov <abduakhatov>
2020-02-25 18:06:23 +0600

    autopush

--- isalpha.go
+++ isalpha.go
@@ -1,9 +1,6 @@
 package piscine
 
 func IsAlpha(str string) bool {
-	// if str == "" {
-	// 	return false
-	// }
 	hasAlpha := false
 	for _, s := range str {
 		if (s >= 48 && s <= 57) || (s >= 65 && s <= 90) || (s >= 97 && s <= 122) {
