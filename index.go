5517d7075cf6cac78930b767d73b048ccbab75c5
abduakhatov <abduakhatov>
2020-02-25 14:24:05 +0600

    q5_t6

--- index.go
+++ index.go
@@ -4,8 +4,9 @@ func Index(s string, toFind string) int {
 	if toFind == "" {
 		return 0
 	}
+	ss := []rune(toFind)
 	bb := []rune(toFind)
-	for ind, val := range s {
+	for ind, val := range ss {
 		if val == bb[0] {
 			return ind
 		}
