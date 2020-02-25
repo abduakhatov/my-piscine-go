102277dfa4185ca199f61d674921727404f69313
abduakhatov <abduakhatov>
2020-02-25 18:32:24 +0600

    autopush

--- isnumeric.go
+++ isnumeric.go
@@ -5,7 +5,7 @@ func IsNumeric(str string) bool {
 	// 	return false
 	// }
 	for _, s := range str {
-		if s < 48 && s > 57 {
+		if s < 48 || s > 57 {
 			return false
 		}
 	}
--- test/main.go
+++ test/main.go
@@ -6,10 +6,8 @@ import (
 )
 
 func main() {
-	
-	fmt.Println(piscine.IsAlpha("Hello! How are you?"))
-	fmt.Println(piscine.IsAlpha("HelloHowareyou"))
-	fmt.Println(piscine.IsAlpha("What's this 4?"))
-	fmt.Println(piscine.IsAlpha("Whatsthis4"))
+
+	fmt.Println(piscine.IsNumeric("010203"))
+	fmt.Println(piscine.IsNumeric("01,02,03"))
 	fmt.Println(piscine.IsAlpha(""))
 }
