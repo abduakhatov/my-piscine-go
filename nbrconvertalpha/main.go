30abe8a37fe8468e487edb351bc51fffa12d4712
abduakhatov <abduakhatov>
2020-02-26 18:42:51 +0600

    autopush

--- nbrconvertalpha/main.go
+++ nbrconvertalpha/main.go
@@ -8,9 +8,11 @@ import (
 func main() {
 	arg := os.Args[1:]
 	charCase := 0
-	if arg != nil && arg[0] == "--upper" {
-		charCase = -32
-		arg = arg[1:]
+	for ind := range arg {
+		if ind == 0 && arg[ind] == "--upper" {
+			charCase = -32
+			arg = arg[1:]
+		}
 	}
 	for _, a := range arg {
 		digit := 0
