12cadf2f82f66e0c7bea6ee0f4fe1c303bac3bdc
abduakhatov <abduakhatov>
2020-02-26 18:35:38 +0600

    autopush

--- flags/main.go
+++ flags/main.go
@@ -1,13 +1,34 @@
 package main
 
-
 import (
-	// "os"
-	// "fmt"
+	"os"
+	"fmt"
 	// "github.com/01-edu/z01"
 )
 
+func PrintHelp() {
+	fmt.Printf(
+		"--insert" + 
+		"\n  -i" +
+		"\n    This flag inserts the string into the string passed as argument."+
+		"\n--order"+
+		"\n  -o"+
+		"\n    This flag will behave like a boolean, if it is called it will order the argument.")
+}
+
+func PrintFlags() {
+	return
+}
+
 func main() {
-	args = os.Args[1:]
-	
+	args := os.Args[1:]
+	fmt.Println(args)
+
+	a := args[0] == []string{}
+
+	if args[0] == "--help" || args[0] == "-h" {
+		PrintHelp()
+	} else {
+		PrintFlags()
+	}
 }
--- nbrconvertalpha/main.go
+++ nbrconvertalpha/main.go
@@ -8,7 +8,7 @@ import (
 func main() {
 	arg := os.Args[1:]
 	charCase := 0
-	if arg[0] == "--upper" {
+	if arg != nil && arg[0] == "--upper" {
 		charCase = -32
 		arg = arg[1:]
 	}
