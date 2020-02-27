6bded4abbf7139393154352822875f1e626f2ebc
abduakhatov <abduakhatov>
2020-02-27 17:37:34 +0600

    autopush

--- split.go
+++ split.go
@@ -1,24 +1,31 @@
 package piscine
 
+import "fmt"
+
 func Split(str, charset string) []string {
 	char_len := get_length(charset)
 	str_len := get_length(str)
 	runes := []rune(str)
 	size := 0
-	for ind := 0; ind < str_len-char_len; ind++ {
+	for ind := 0; ind <= str_len-char_len; ind++ {
 		if string(runes[ind:ind+char_len]) == charset {
 			size++
 		}
 	}
+	fmt.Println(size)
 	resArr := make([]string, size+1)
 	i := 0
 	start := 0
 	ind := 0
-	for ; ind < str_len-char_len; ind++ {
+	for ; ind <= str_len-char_len; ind++ {
+		fmt.Println(str_len, "/", ind, "+", char_len, string(runes[ind:ind+char_len]))
 		if string(runes[ind:ind+char_len]) == charset {
 			resArr[i] = string(runes[start:ind])
 			i++
 			start = ind + char_len
+		} 
+		if ind == str_len - char_len - 1 { 
+			ind += char_len
 		}
 	}
 	resArr[i] = string(runes[start:ind])
