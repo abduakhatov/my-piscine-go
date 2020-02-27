88777db324b0b1f3ed116e313f1195c787e4ac39
abduakhatov <abduakhatov>
2020-02-27 19:12:49 +0600

    autopush

--- split.go
+++ split.go
@@ -5,10 +5,10 @@ package piscine
 func Split(str, charset string) []string {
 	char_len := get_length(charset)
 	str_len := get_length(str)
-	runes := []rune(str)
+	// runes := []rune(str)
 	size := 0
 	for ind := 0; ind <= str_len-char_len; ind++ {
-		if string(runes[ind:ind+char_len]) == charset {
+		if string(str[ind:ind+char_len]) == charset {
 			size++
 		}
 	}
@@ -19,15 +19,16 @@ func Split(str, charset string) []string {
 	ind := 0
 	for ; ind <= str_len-char_len; ind++ {
 		// fmt.Println(str_len, "/", ind, "+", char_len, string(runes[ind:ind+char_len]))
-		if string(runes[ind:ind+char_len]) == charset {
-			resArr[i] = string(runes[start:ind])
+		if string(str[ind:ind+char_len]) == charset {
+			resArr[i] = string(str[start:ind])
 			i++
 			start = ind + char_len
 		}
 		if ind == str_len-char_len {
-			ind += char_len
+			// ind += char_len
+			resArr[i] = string(str[start:])
 		}
 	}
-	resArr[i] = string(runes[start:ind])
+	// resArr[i] = string(str[start:ind])
 	return resArr
 }
--- test/main.go
+++ test/main.go
@@ -6,6 +6,9 @@ import (
 )
 
 func main() {
-	// str := "HelloHAyou?"
-	fmt.Println(piscine.Split("3jAMMmyZRtdWH|<=>|OawUzNGSYI9X3|<=>|YsPqJKoymzJY9|<=>|Xyt0rrVYcDdhs|<=>|EvcDPwtEBtkor|<=>|1nTmC8yQS9MGA|<=>|86LXgV5vfE2pU|<=>|sS4D9nBnvYOHa|<=>|W7fBtIBY78RRj|<=>|nslaWO7EWS28k|<=>|G21qjwQ7C5HUJ|<=>|WSVGEJswmAi59|<=>|vvJi51F3GXT6o", "|<=>|"))
+	piscine := piscine.Split("8JvVwU93tLkrUDIDVFvcIIB24BQuGDIDj90biLjB03MUeDID0J2yFPJ2AQGnxDIDW1ESC9b6UTkvwDIDuSCNbksPp8XqWDIDJcUJ6MuflChIuDIDKbkjA6a6LzgkcDIDK9O8pI0h9UN6bDIDFfxkzD7PcvWtTDIDkukH5Xeiv2132DID2VpaJfg8AUKAzDIDwGar4ZfFryKJE", "DID")
+	fmt.Println(piscine)
+	fmt.Println(piscine[12], len(piscine[12]))
+	fmt.Println(piscine[11], len(piscine[11]))
+	
 }
