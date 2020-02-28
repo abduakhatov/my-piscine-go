899ab7641b4eb48b8f0ab0083e3e1489383d6b54
shohruh_abduakhatov <shokhrukh.abduahadov@gmail.com>
2020-02-28 18:51:56 +0500

    autopush

--- printnbrbase.go
+++ printnbrbase.go
@@ -1,2 +1,35 @@
 package piscine
 
+import (
+	"github.com/01-edu/z01"
+)
+
+func ParseToBase(nbr int, base []rune, size int, sign int) {
+	if nbr < size {
+		z01.PrintRune(base[int(nbr*sign)])
+		return
+	}
+	ParseToBase(nbr/size, base, size, sign)
+	z01.PrintRune(base[int(nbr%size*sign)])
+}
+
+func PrintNbrBase(nbr int, base string) {
+	size := 0
+	// isDifferent := false
+	for range base {
+		size++
+	}
+	if size < 2 {
+		z01.PrintRune('N')
+		z01.PrintRune('V')
+		return
+	}
+	// for i := 0; i < size-1; i++ {
+	// }
+	dot := 1
+	if nbr < 0 {
+		dot *= -1
+		z01.PrintRune('-')
+	}
+	ParseToBase(nbr, []rune(base), size, dot)
+}
--- test/main.go
+++ test/main.go
@@ -1,7 +1,19 @@
 package main
 
-import piscine ".."
+import (
+	piscine ".."
+	"github.com/01-edu/z01"
+)
 
 func main() {
-	piscine.EightQueens()
+	piscine.PrintNbrBase(125, "0123456789")
+	z01.PrintRune('\n')
+	piscine.PrintNbrBase(-125, "01")
+	z01.PrintRune('\n')
+	// piscine.PrintNbrBase(125, "0123456789ABCDEF")
+	// z01.PrintRune('\n')
+	// piscine.PrintNbrBase(-125, "choumi")
+	// z01.PrintRune('\n')
+	// piscine.PrintNbrBase(125, "aa")
+	// z01.PrintRune('\n')
 }
