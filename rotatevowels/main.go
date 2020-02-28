f506890c2dce1c0f6c18541cd7a872d7b72ea57b
shohruh_abduakhatov <shokhrukh.abduahadov@gmail.com>
2020-02-28 12:01:42 +0500

    autopush

--- rotatevowels/main.go
+++ rotatevowels/main.go
@@ -1,9 +1,31 @@
 package main
 
 import (
+	"os"
+
 	"github.com/01-edu/z01"
 )
 
 func main() {
-	z01.PrintRune('1')
+	args := os.Args[1:]
+	str := " "
+	for _, val := range args {
+		str = str + val + " "
+	}
+	str = str[1:]
+	indexes := []int{}
+	length := 0
+	for ind, val := range str {
+		if val == 'a' || val == 'e' || val == 'i' || val == 'o' || val == 'u' || val == 'A' || val == 'E' || val == 'I' || val == 'O' || val == 'U' {
+			indexes = append(indexes, ind)
+			length++
+		}
+	}
+	strRune := []rune(str)
+	for i := 0; i <= length/2 && length != 0; i++ {
+		strRune[indexes[i]], strRune[indexes[length-i-1]] = strRune[indexes[length-i-1]], strRune[indexes[i]]
+	}
+	for _, val := range strRune {
+		z01.PrintRune(val)
+	}
 }
