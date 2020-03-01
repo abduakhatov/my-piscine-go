ed8d92ccf82289b6447d2b6f3541ec47abf6267f
abduakhatov <abduakhatov>
2020-03-02 00:39:14 +0600

    autopush

--- convertbase.go
+++ convertbase.go
@@ -1,40 +1,25 @@
 package piscine
 
-// func ToNbrBase(nbr int, base string) string {
-// 	size := 0
-// 	isValid := true
-// 	for _, val := range base {
-// 		if val == '-' || val == '+' {
-// 			isValid = false
-// 			size = 0
-// 			break
-// 		}
-// 		size++
-// 	}
-// 	if size < 2 {
-// 		return ""
-// 	}
-// 	for i := 0; i < size-1; i++ {
-// 		for j := i + 1; j < size; j++ {
-// 			if base[j] == base[i] {
-// 				isValid = false
-// 				break
-// 			}
-// 		}
-// 	}
-// 	if !isValid {
-// 		return ""
-// 	}
-// 	dot := 1
-// 	if nbr < 0 {
-// 		dot *= -1
-// 		z01.PrintRune('-')
-// 	}
-// 	ParseToBase(nbr, []rune(base), size, dot)
-// }
+
+import "fmt"
 
 
+func ToBaseRec(nbr int, base []rune, size int) string {
+	if nbr < size {
+		return string(base[nbr])
+	}
+	return ToBaseRec(nbr/size, base, size) + string(base[nbr%size])
+}
+
+func ToBase(nbr int, base string) string {
+	size := 0
+	for range base {
+		size++
+	}
+	return ToBaseRec(nbr, []rune(base), size)
+}
 
 func ConvertBase(nbr, baseFrom, baseTo string) string {
-	return "Hello"
+	number := AtoiBase(nbr, baseFrom)
+	return ToBase(number, baseTo)
 }
--- test/main.go
+++ test/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	piscine ".."
+	"fmt"
 )
 
 func main() {
-	piscine.EightQueens()
+	result := piscine.ConvertBase("101011", "01", "0123456789")
+	fmt.Println(result)
 }
