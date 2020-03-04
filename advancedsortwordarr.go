5b3fa3c4be900834e759fb923c67470a50512325
abduakhatov <abduakhatov>
2020-03-05 03:16:07 +0600

    autopush

--- advancedsortwordarr.go
+++ advancedsortwordarr.go
@@ -1,13 +1,17 @@
 package piscine
 
-// func AdvancedSortWordArr(array []string, f func(a, b string) int) {
-// 	for i := 0; i < len(*array)-1; i++ {
-// 		for j := i + 1; j < len(*array); j++ {
-// 			if f((*array)[i], (*array)[j]) > 0 {
-// 				tmp := (*array)[i]
-// 				(*array)[i] = (*array)[j]
-// 				(*array)[j] = tmp
-// 			}
-// 		}
-// 	}
-// }
+func AdvancedSortWordArr(array []string, f func(a, b string) int) {
+	length := 0
+	for range array {
+		length++
+	}
+	for i := 0; i < length-1; i++ {
+		for j := i + 1; j < length; j++ {
+			if f(array[i], array[j]) > 0 {
+				tmp := array[i]
+				array[i] = array[j]
+				array[j] = tmp
+			}
+		}
+	}
+}
--- test/main.go
+++ test/main.go
@@ -8,7 +8,7 @@ import (
 func main() {
 
 	result := []string{"a", "A", "1", "b", "B", "2", "c", "C", "3"}
-	piscine.SortWordArr(result)
+	piscine.AdvancedSortWordArr(result, piscine.Compare)
 
 	fmt.Println(result)
 }
