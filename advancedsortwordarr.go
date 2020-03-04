c13c733d217f7e482bb67ba2dcc5677e23621d3b
abduakhatov <abduakhatov>
2020-03-05 03:39:26 +0600

    autopush

--- advancedsortwordarr.go
+++ advancedsortwordarr.go
@@ -5,14 +5,14 @@ func AdvancedSortWordArr(array []string, f func(a, b string) int) {
 	for range array {
 		length++
 	}
-	
-	// for i := 0; i < length-1; i++ {
-	// 	for j := i + 1; j < length; j++ {
-	// 		if f(array[i], array[j]) > 0 {
-	// 			tmp := array[i]
-	// 			array[i] = array[j]
-	// 			array[j] = tmp
-	// 		}
-	// 	}
-	// }
+	for i := 0; i < length-1; i++ {
+		for j := i + 1; j < length; j++ {
+			if f(array[i], array[j]) > 0 {
+				tmp := array[i]
+				array[i] = array[j]
+				array[j] = tmp
+			}
+		}
+	}
 }
+
