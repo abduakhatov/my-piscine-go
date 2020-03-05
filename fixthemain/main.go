5606795644ed2848d8f6da09cdbb250f13e18c1d
abduakhatov <abduakhatov>
2020-03-05 12:04:31 +0600

    autopush

--- fixthemain/main.go
+++ fixthemain/main.go
@@ -21,24 +21,24 @@ func PrintStr(str string) {
 }
 
 func OpenDoor(ptrDoor *Door) bool {
-	PrintStr("Door Opening...")
+	PrintStr("Door Opening...\n")
 	ptrDoor.state = OPEN
 	return true
 }
 
 func CloseDoor(ptrDoor *Door) bool {
-	PrintStr("Door Closing...")
+	PrintStr("Door Closing...\n")
 	ptrDoor.state = CLOSE
 	return true
 }
 
 func IsDoorOpen(ptrDoor *Door) bool {
-	PrintStr("is the Door opened ?")
+	PrintStr("is the Door opened ?\n")
 	return ptrDoor.state == OPEN
 }
 
 func IsDoorClose(ptrDoor *Door) bool {
-	PrintStr("is the Door closed ?")
+	PrintStr("is the Door closed ?\n")
 	return ptrDoor.state == CLOSE
 }
 
