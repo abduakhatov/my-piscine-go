479ceb3929b51f151a03a2bdc8c928591369a127
abduakhatov <abduakhatov>
2020-03-10 01:34:27 +0600

    autopush

--- listpushfront.go
+++ listpushfront.go
@@ -1,17 +1,10 @@
 package piscine
 
-// type NodeL struct {
-// 	Data interface{}
-// 	Next *NodeL
-// }
-
-// type List struct {
-// 	Head *NodeL
-// 	Tail *NodeL
-// }
-
 func ListPushFront(l *List, data interface{}) {
 	n := &NodeL{Data: data}
+	if l.Tail == nil {
+		l.Tail = n
+	}
 	n.Next = l.Head
 	l.Head = n
 }
