41e8cd399af2160b0db40df8e65d9dd48d0a4766
abduakhatov <abduakhatov>
2020-03-10 01:41:27 +0600

    autopush

--- test/main.go
+++ test/main.go
@@ -10,9 +10,14 @@ func main() {
 	link := &piscine.List{}
 	link2 := &piscine.List{}
 
-	piscine.ListPushBack(link, "three")
-	piscine.ListPushBack(link, 3)
-	piscine.ListPushBack(link, "1")
+	// piscine.ListPushBack(link, "three")
+	// piscine.ListPushBack(link, 3)
+	// piscine.ListPushBack(link, "1")
+
+	piscine.ListPushBack(link, "-2381620042740041718")
+	piscine.ListPushBack(link, "-2381620042740041718")
+	piscine.ListPushBack(link, "1223605871337147087")
+	piscine.ListPushBack(link, "3026360979856967082")
 
 	fmt.Println(piscine.ListLast(link))
 	fmt.Println(piscine.ListLast(link2))
