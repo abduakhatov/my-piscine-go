4f18b908050f4a468c39c8d0e4837b2178c5bcf8
abduakhatov <abduakhatov>
2020-03-10 18:36:57 +0600

    autopush

--- test/main.go
+++ test/main.go
@@ -1,47 +1,20 @@
 package main
 
 import (
-	piscine ".."
 	"fmt"
+	piscine ".."
 )
-
-func PrintElem(node *piscine.NodeL) {
-	fmt.Println(node.Data)
-}
-
-func StringToInt(node *piscine.NodeL) {
-	node.Data = 2
-}
-
-func PrintList(l *piscine.List) {
-	it := l.Head
-	for it != nil {
-		fmt.Print(it.Data, "->")
-		it = it.Next
-	}
-	fmt.Print("nil","\n")
-}
 
 func main() {
 	link := &piscine.List{}
 
-	piscine.ListPushBack(link, 1)
 	piscine.ListPushBack(link, "hello")
-	piscine.ListPushBack(link, 3)
-	piscine.ListPushBack(link, "there")
-	piscine.ListPushBack(link, 23)
-	piscine.ListPushBack(link, "!")
-	piscine.ListPushBack(link, 54)
+	piscine.ListPushBack(link, "hello1")
+	piscine.ListPushBack(link, "hello2")
+	piscine.ListPushBack(link, "hello3")
 
-	PrintList(link)
+	found := piscine.ListFind(link, interface{}("hello2"), piscine.CompStr)
 
-	fmt.Println("--------function applied--------")
-	piscine.ListForEachIf(link, PrintElem, piscine.IsPositive_node)
-
-	piscine.ListForEachIf(link, StringToInt, piscine.IsNotNumeric_node)
-
-	fmt.Println("--------function applied--------")
-	PrintList(link)
-
-	fmt.Println()
+	fmt.Println(found)
+	fmt.Println(*found)
 }
