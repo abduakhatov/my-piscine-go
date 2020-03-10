fe9a74ca4f961eb1e3e76a9daabda64f21f241a8
abduakhatov <abduakhatov>
2020-03-10 19:55:07 +0600

    autopush

--- test/main.go
+++ test/main.go
@@ -12,7 +12,6 @@ func PrintList(l *piscine.List) {
 		fmt.Print(it.Data, " -> ")
 		it = it.Next
 	}
-
 	fmt.Print(nil, "\n")
 }
 
@@ -20,29 +19,21 @@ func main() {
 	link := &piscine.List{}
 	link2 := &piscine.List{}
 
-	fmt.Println("----normal state----")
-	piscine.ListPushBack(link2, 1)
-	PrintList(link2)
-	piscine.ListRemoveIf(link2, 1)
-	fmt.Println("------answer-----")
-	PrintList(link2)
-	fmt.Println()
-
-	fmt.Println("----normal state----")
-	piscine.ListPushBack(link, 1)
-	piscine.ListPushBack(link, "Hello")
-	piscine.ListPushBack(link, 1)
-	piscine.ListPushBack(link, "There")
-	piscine.ListPushBack(link, 1)
-	piscine.ListPushBack(link, 1)
-	piscine.ListPushBack(link, "How")
-	piscine.ListPushBack(link, 1)
-	piscine.ListPushBack(link, "are")
-	piscine.ListPushBack(link, "you")
-	piscine.ListPushBack(link, 1)
+	piscine.ListPushBack(link, "a")
+	piscine.ListPushBack(link, "b")
+	piscine.ListPushBack(link, "c")
+	piscine.ListPushBack(link, "d")
+	fmt.Println("-----first List------")
 	PrintList(link)
 
-	piscine.ListRemoveIf(link, 1)
-	fmt.Println("------answer-----")
+	piscine.ListPushBack(link2, "e")
+	piscine.ListPushBack(link2, "f")
+	piscine.ListPushBack(link2, "g")
+	piscine.ListPushBack(link2, "h")
+	fmt.Println("-----second List------")
+	PrintList(link2)
+
+	fmt.Println("-----Merged List-----")
+	piscine.ListMerge(link, link2)
 	PrintList(link)
 }
