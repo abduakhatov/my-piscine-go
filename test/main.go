09fdcb5b4083a45a7023dc2e9690880c41e98f0d
abduakhatov <abduakhatov>
2020-03-11 15:53:24 +0600

    autopush

--- test/main.go
+++ test/main.go
@@ -6,9 +6,19 @@ import (
 )
 
 func main() {
-	root := &piscine.TreeNode{Data: "4"}
-	piscine.BTreeInsertData(root, "1")
-	piscine.BTreeInsertData(root, "7")
-	piscine.BTreeInsertData(root, "5")
-	fmt.Println(piscine.BTreeIsBinary(root))
+	root := &piscine.TreeNode{Data: "40"}
+	piscine.BTreeInsertData(root, "07")
+	piscine.BTreeInsertData(root, "12")
+	piscine.BTreeInsertData(root, "05")
+	piscine.BTreeInsertData(root, "10")
+
+	piscine.BTreeInsertData(root, "01")
+	piscine.BTreeInsertData(root, "02")
+	piscine.BTreeInsertData(root, "03")
+	piscine.BTreeInsertData(root, "00")
+	root.Left.Left.Data = "123"
+
+	fmt.Println(root.Left.Left.Data)
+
+	// fmt.Println(piscine.BTreeIsBinary(root))
 }
