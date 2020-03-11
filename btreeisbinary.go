26331836926a9973c246b3086d6d2bfd1cd2d4f3
abduakhatov <abduakhatov>
2020-03-11 15:49:11 +0600

    autopush

---
+++ btreeisbinary.go
@@ -0,0 +1,15 @@
+package piscine
+
+func BTreeIsBinary(root *TreeNode) bool {
+	if root == nil {
+		return true
+	}
+	if root.Parent != nil {
+		if root.Parent.Data <= root.Data && root == root.Parent.Left {
+			return false
+		} else if root.Parent.Data > root.Data && root == root.Parent.Right {
+			return false
+		}
+	}
+	return BTreeIsBinary(root.Left) && BTreeIsBinary(root.Right)
+}
--- test/main.go
+++ test/main.go
@@ -10,32 +10,5 @@ func main() {
 	piscine.BTreeInsertData(root, "1")
 	piscine.BTreeInsertData(root, "7")
 	piscine.BTreeInsertData(root, "5")
-	selected := piscine.BTreeSearchItem(root, "7")
-	fmt.Print("Item selected -> ")
-	if selected != nil {
-		fmt.Println(selected.Data)
-	} else {
-		fmt.Println("nil")
-	}
-
-	fmt.Print("Parent of selected item -> ")
-	if selected.Parent != nil {
-		fmt.Println(selected.Parent.Data)
-	} else {
-		fmt.Println("nil")
-	}
-
-	fmt.Print("Left child of selected item -> ")
-	if selected.Left != nil {
-		fmt.Println(selected.Left.Data)
-	} else {
-		fmt.Println("nil")
-	}
-
-	fmt.Print("Right child of selected item -> ")
-	if selected.Right != nil {
-		fmt.Println(selected.Right.Data)
-	} else {
-		fmt.Println("nil")
-	}
+	fmt.Println(piscine.BTreeIsBinary(root))
 }
