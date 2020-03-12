8b73e40018d60757ace141e803fd5c0a1cf42e3e
abduakhatov <abduakhatov>
2020-03-12 17:59:17 +0600

    autopush

--- btreemin.go
+++ btreemin.go
@@ -4,15 +4,8 @@ func BTreeMin(root *TreeNode) *TreeNode {
 	if root == nil {
 		return nil
 	}
-	max := &TreeNode{Data: root.Data}
-	left := BTreeMax(root.Left)
-	right := BTreeMax(root.Right)
-
-	if left != nil && BtreeAtoi(left.Data) < BtreeAtoi(root.Data) {
-		max = &TreeNode{Data: left.Data}
+	if root.Left == nil {
+		return root
 	}
-	if right != nil && BtreeAtoi(right.Data) < BtreeAtoi(root.Data) {
-		max = &TreeNode{Data: right.Data}
-	}
-	return max
+	return BTreeMin(root.Left)
 }
--- test/main.go
+++ test/main.go
@@ -46,67 +46,11 @@ func main() {
 		piscine.BTreeInsertData(root, "10")
 	
 		piscine.BTreeInsertData(root, "01")
-		piscine.BTreeInsertData(root, "02")
 		piscine.BTreeInsertData(root, "03")
+		piscine.BTreeInsertData(root, "04")
+		piscine.BTreeInsertData(root, "00")
+		min := piscine.BTreeMin(root)
 	max := piscine.BTreeMax(root)
-	fmt.Println(max.Data)
+	fmt.Println(max.Data, min.Data)
 }
 
-
-// package main
-
-// import "fmt"
-
-// type node struct {
-//     value string
-//     left  *node
-//     right *node
-// }
-
-// func insert(n *node, v string) *node {
-//     if n == nil {
-//         return &node{v, nil, nil}
-//     } else if v <= n.value {
-//         n.left = insert(n.left, v)
-//     } else {
-//         n.right = insert(n.right, v)
-//     }
-//     return n
-// }
-
-
-// /* breadth first traversal */
-// func breadth(n *node) {
-//     if n != nil {
-// 		s := []*node{n}
-
-// 		fmt.Print(s[0])
-// 		fmt.Print(s[1])
-// 		fmt.Print(s[2])
-
-//         for len(s) > 0 {
-//             current_node := s[0]
-//             fmt.Printf(current_node.value + " ")
-//             s = s[1:]
-//             if current_node.left != nil {
-//                 s = append(s, current_node.left)
-//             }
-//             if current_node.right != nil {
-//                 s = append(s, current_node.right)
-//             }
-//         }
-//     }
-// }
-
-// func main() {
-//     var root *node
-//     root = insert(root, "F")
-//     root = insert(root, "D")
-//     root = insert(root, "H")
-//     root = insert(root, "B")
-//     root = insert(root, "E")
-//     root = insert(root, "G")
-//     root = insert(root, "J")
-//     fmt.Println("\nBFS traversal")
-//     breadth(root)
-// }
