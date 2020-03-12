b886f4aed9a9bb136c176ed6ec36cc1041d45c3e
abduakhatov <abduakhatov>
2020-03-12 18:12:17 +0600

    autopush

--- btreetransplant.go
+++ btreetransplant.go
@@ -4,8 +4,13 @@ func BTreeTransplant(root, node, rplc *TreeNode) *TreeNode {
 	if root == nil {
 		return nil
 	}
-	if root == node {
-		root = rplc
+	if root.Left != nil && root.Left == node {
+		root.Left = node
+		root.Left.Parent = root
+
+	} else if root.Right != nil && root.Right == node {
+		root.Right = node
+		root.Right.Parent = root
 	} else {
 		root.Left = BTreeTransplant(root.Left, node, rplc)
 		root.Right = BTreeTransplant(root.Right, node, rplc)
